eb1b3b8b69f35beff8dc16a92fd80ce6add89d0d
luka onik <luka.onikadze@toptal.com>
2024-01-11 18:33:36 +0400

    select added

--- src/app/features/test-page/test-page.component.ts
+++ src/app/features/test-page/test-page.component.ts
@@ -1,19 +1,21 @@
 import { Component, signal } from '@angular/core';
 import { ButtonComponent } from '../../core/button/button.component';
+import { SelectComponent } from '../../core/select/select.component';
+import { SelectItemComponent } from '../../core/select/select-item/select-item.component';
 
 @Component({
   selector: 'app-test-page',
   standalone: true,
-  imports: [ButtonComponent],
+  imports: [ButtonComponent, SelectComponent, SelectItemComponent],
   templateUrl: './test-page.component.html',
-  styleUrl: './test-page.component.scss'
+  styleUrl: './test-page.component.scss',
 })
 export class TestPageComponent {
-  booleanValue=signal(true);
+  booleanValue = signal(true);
 
-  constructor(){
-    setTimeout(()=>{
+  constructor() {
+    setTimeout(() => {
       // this.booleanValue.set(false)
-    },5000)
+    }, 5000);
   }
 }
