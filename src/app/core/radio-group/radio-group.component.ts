649d5d6c123bcea7feb5c21e7409700c6a7d08b0
luka onik <luka.onikadze@toptal.com>
2024-01-11 20:40:54 +0400

    radio button update

--- src/app/core/radio-group/radio-group.component.ts
+++ src/app/core/radio-group/radio-group.component.ts
@@ -2,9 +2,11 @@ import {
   ChangeDetectionStrategy,
   Component,
   Input,
+  forwardRef,
   inject,
 } from '@angular/core';
 import { RadioGroupState } from './radio-group.state';
+import { ControlValueAccessor, NG_VALUE_ACCESSOR } from '@angular/forms';
 
 @Component({
   selector: 'app-radio-group',
@@ -13,10 +15,31 @@ import { RadioGroupState } from './radio-group.state';
   templateUrl: './radio-group.component.html',
   styleUrl: './radio-group.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
-  providers: [RadioGroupState],
+  providers: [
+    RadioGroupState,
+    {
+      provide: NG_VALUE_ACCESSOR,
+      multi: true,
+      useExisting: forwardRef(() => RadioGroupComponent),
+    },
+  ],
 })
-export class RadioGroupComponent {
+export class RadioGroupComponent implements ControlValueAccessor {
   radioGroupState = inject(RadioGroupState);
+
+  writeValue(obj: any): void {
+    this.radioGroupState.writeValue(obj);
+  }
+  registerOnChange(fn: (_: any) => void): void {
+    this.radioGroupState.onChange = fn;
+  }
+  registerOnTouched(fn: () => void): void {
+    this.radioGroupState.onTouched = fn;
+  }
+
+  setDisabledState(disabled: boolean): void {
+    this.radioGroupState.setDisabled(disabled);
+  }
 
   @Input()
   set compareWith(compareWith: any) {
